213ed7b9a53c29bb2ef4b9fd3a9d843e5ede4d70
Jean KONAN <jean.konan@epitech.eu>
2026-05-18 18:20:57 +0000

    Intro update profile

--- portfolio-vue/src/components/AppFooter.vue
+++ portfolio-vue/src/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="row">
       <div class="column large-full ss-copyright">
-        <span>© Copyright Jean Kouassi. {{ new Date().getFullYear() }}</span>
+        <span>© Copyright Jean KONAN. {{ new Date().getFullYear() }}</span>
       </div>
       <div class="ss-go-top">
         <a class="smoothscroll" title="Retour en haut" href="#top" @click.prevent="scrollTop"></a>
